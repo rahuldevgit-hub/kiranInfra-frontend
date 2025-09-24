38582cfb545960681ca165999427be82900afd73
rahuldevgit-hub <choudharyrahul7600@gmail.com>
2025-09-24 17:48:02 +0530

    Update next.config.ts output enables

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  output: 'export',       // ✅ enables static export
   images: {
-    // domains: ["newstaging.kiraninfra.com"],
     domains: ["192.168.0.65"],
   },
+  // optional: trailingSlash: true, // uncomment if you want URLs to end with '/'
 };
 
 export default nextConfig;
